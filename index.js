40c2e9bd13eb492862b5280967c139b1d7302470
Héber Lima Silva <heberlimasilva@gmail.com>
2024-02-08 15:15:54 -0300

    adding pdf file reading from AI

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 require("dotenv").config();
 
-const sendPrompt = async () => {
+
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
+const filePath = 'modelo-atestado.pdf';
+
+fs.readFile(filePath, async (err, data) => {
+    if (err) {
+        console.error('Erro ao ler o arquivo PDF:', err);
+        return;
+    }
+
+    const options = {};  // Você pode fornecer opções adicionais aqui, se necessário
+
+    try {
+        const pdfData = await pdf(data, options);
+        //console.log(pdfData.text);
+        await sendPrompt(pdfData.text);
+    } catch (error) {
+        console.error('Erro ao processar o arquivo PDF:', error);
+    }
+});
+
+
+const sendPrompt = async (pdf) => {
     
     const client = new OpenAIClient(
         "https://" + process.env.RESOURCE_NAME + ".openai.azure.com/", 
@@ -10,17 +34,15 @@ const sendPrompt = async () => {
       
     try {
       const messages = [
-        { role: "system", content: "You are a helpful assistant. You will talk like a pirate." },
-        { role: "user", content: "Can you help me?" },
-        { role: "assistant", content: "Arrrr! Of course, me hearty! What can I do for ye?" },
-        { role: "user", content: "What's the best way to train a parrot?" },
+        { role: "system", content: "What information does the following pdf file provide?" },
+        { role: "user", content: pdf },
+       
       ];
     
       const response = await client.getChatCompletions(process.env.DEPLOYMENT_ID, messages, { maxTokens: 128 });
-      console.log(response)
+      console.log(response.choices[0].message.content)
+      //console.log(response)
     } catch (error) {
       console.log(error)
     }
 };
-
-sendPrompt();
